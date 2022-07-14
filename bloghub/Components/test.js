cf5361df20e8ba2b0b71084726eaf4680b6c991f
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-14 17:00:27 +0500

    Inserted Components/test.js at 14/07/2022 17:00:27

---
+++ bloghub/Components/test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <>
+        <style jsx>
+            {`
+                .main{
+                    background-color: red;
+                }
+            `}
+        </style>
+        <div className='main'>Test</div>
+    </>
+  )
+}
+
+export default Test

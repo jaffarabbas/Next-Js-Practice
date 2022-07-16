98274785e6cce3dfc75b8255d3e753d9084bc45e
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-16 22:42:24 +0500

    Updated pages/_app.js at 16/07/2022 22:42:24

--- bloghub/pages/_app.js
+++ bloghub/pages/_app.js
@@ -1,7 +1,14 @@
+import Navbar from '../Components/Navbar'
+import Link from 'next/link'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Navbar/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp

d40e47588a10dfae4947f5128390cf6ad4fa01e2
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-16 15:40:43 +0500

    Update about.js

--- bloghub/pages/about.js
+++ bloghub/pages/about.js
@@ -3,14 +3,6 @@ import React from 'react'
 const About = () => {
   return (
     <>
-        <style jsx>
-            {`
-                .main{
-                    background-color: red;
-                    font-size: 20px;
-                }
-            `}
-        </style>
         <div className='main'>about</div>
     </>
   )

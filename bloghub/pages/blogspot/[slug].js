463d33430efa7bc482aa0fa8bc522dfe4b32ca02
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-16 22:42:24 +0500

    Updated pages/blogspot/[slug].js at 16/07/2022 22:42:24

--- bloghub/pages/blogspot/[slug].js
+++ bloghub/pages/blogspot/[slug].js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import styles from '../../styles/BlogPost.module.css'
 
 const slug = () => {
     const router = useRouter()
     const { slug } = router.query
     return (
-        <div>{slug}</div>
+        <>
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    <h1>Title : {slug}</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, recusandae. Voluptatibus quasi maxime pariatur eum ea quas id rerum quaerat illo. Nesciunt libero ad beatae aliquid amet non tempore ea.</p>
+                </main>
+            </div>
+        </>
     )
 }
 

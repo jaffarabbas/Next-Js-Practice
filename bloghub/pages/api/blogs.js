8a78f19bcfe5c0aac95b453df00c889c0c1a723f
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-18 11:59:28 +0500

    Update blogs.js

--- bloghub/pages/api/blogs.js
+++ bloghub/pages/api/blogs.js
@@ -2,8 +2,7 @@
 import * as fs from 'fs';
 
 export default function handler(req, res) {
-  fs.readFile('blogdata/htlf.json','utf-8',(err, data) => { 
-    console.log(data);
-  });
-  res.status(200).json({ name: 'John Doe' })
+  fs.readdir('blogdata','utf-8',(err, data) => { 
+    res.status(200).json( data )
+  })
 }

5d27c5125e7be5c3db3cc780b9be105342805476
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-20 14:39:41 +0500

    Commited bloghub/pages/api/blogs.js bloghub/pages/api/blogs.js at 20-07-2022 02:39:40

--- bloghub/pages/api/blogs.js
+++ bloghub/pages/api/blogs.js
@@ -1,8 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from 'fs';
 
-export default function handler(req, res) {
-  fs.readdir('blogdata','utf-8',(err, data) => { 
-    res.status(200).json( data )
-  })
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir('blogdata');
+  let file;
+  let allBlogs = [];
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    file = await fs.promises.readFile(('blogdata/' + item), 'utf-8');
+    allBlogs.push(JSON.parse(file));
+  }
+  res.status(200).json(allBlogs);
 }

addaf61a872b5f126db6925cff7a29401106c7d1
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-20 14:39:41 +0500

    Commited bloghub/pages/blog.js bloghub/pages/blog.js at 20-07-2022 02:39:41

--- bloghub/pages/blog.js
+++ bloghub/pages/blog.js
@@ -1,33 +1,32 @@
 import React from 'react'
+import { useEffect , useState } from 'react'
 import Link from 'next/link'
 import styles from '../styles/Blog.module.css'
 
 const Blog = () => {
+  const [blog, setBlog] = useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:3000/api/blogs').then((allData)=>{
+      return allData.json();
+    }).then((blog) => {
+      setBlog(blog);
+    });
+  },[]);
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <div className={styles.blog}>
           <h1>Popular Blog</h1>
-          <Link href={'/blogspot/Maintaining-Nextjs-App'}>
-            <div className="blogcontainer">
-              <h3>Maintaining Nextjs App</h3>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum commodi labore molestiae, optio tempora laboriosam. Voluptatem soluta vitae ipsum quam repellendus quo repellat ducimus quibusdam! Eos cumque voluptatem natus eius.</p>
-            </div>
-          </Link>
-          <div className="blogcontainer">
-            <h3>Maintaining Nextjs App</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum commodi labore molestiae, optio tempora laboriosam. Voluptatem soluta vitae ipsum quam repellendus quo repellat ducimus quibusdam! Eos cumque voluptatem natus eius.</p>
-          </div>
-          <div className="blogcontainer">
-            <h3>Maintaining Nextjs App</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum commodi labore molestiae, optio tempora laboriosam. Voluptatem soluta vitae ipsum quam repellendus quo repellat ducimus quibusdam! Eos cumque voluptatem natus eius.</p>
-          </div><div className="blogcontainer">
-            <h3>Maintaining Nextjs App</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum commodi labore molestiae, optio tempora laboriosam. Voluptatem soluta vitae ipsum quam repellendus quo repellat ducimus quibusdam! Eos cumque voluptatem natus eius.</p>
-          </div><div className="blogcontainer">
-            <h3>Maintaining Nextjs App</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum commodi labore molestiae, optio tempora laboriosam. Voluptatem soluta vitae ipsum quam repellendus quo repellat ducimus quibusdam! Eos cumque voluptatem natus eius.</p>
-          </div>
+          {
+            blog.map((blog) => {
+               return <Link href={`/blogspot/${blog.slug}`} key={blog.title}>
+               <div className="blogcontainer">
+                 <h3>{blog.title}</h3>
+                 <p>{blog.description.substr(0,100)}...</p>
+               </div>
+             </Link> 
+            })
+          }
         </div>
       </main>
     </div>

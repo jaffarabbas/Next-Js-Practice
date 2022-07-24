bf1192e382bfed67763b404afa956b0f54226cc5
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-24 21:43:12 +0500

    Commited File : TodoService.js at 7/24/2022 9:43:11 PM

---
+++ nextcrudapp-firebase/services/TodoService.js
@@ -0,0 +1,29 @@
+import { collection, onSnapshot, orderBy, query, addDoc,serverTimestamp, deleteDoc, doc} from 'firebase/firestore';
+import { db } from "../firebase";
+
+class TodoService {
+    getTodo = (collectionName,setTodos) => {
+        let collectionRef = collection(db, collectionName);
+        const querys = query(collectionRef, orderBy("timestamp", "desc"));
+        const data = onSnapshot(querys, (snapShot) => {
+            setTodos(snapShot.docs.map(doc => ({
+                ...doc.data(),
+                id: doc.id,
+                timestamp: doc.data().timestamp?.toDate().getTime()
+            })));
+            return data;
+        });
+    }
+    postTodo = async (collectionName, state) => {
+        let collectionRef = collection(db, collectionName);
+        const docRef = await addDoc(collectionRef, { ...state, timestamp: serverTimestamp() });
+        return docRef;
+    }
+
+    deleteTodo = async (collectionName, id) => {
+        let docRef = doc(db, collectionName,id);
+        await deleteDoc(docRef);
+    }
+}
+
+export default new TodoService();

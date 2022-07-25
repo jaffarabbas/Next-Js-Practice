4b6c227c0358979f5ba7a92911865ca15ad1a586
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-25 20:18:44 +0500

    Commited File : [id].js at 7/25/2022 8:18:43 PM

---
+++ nextcrudapp-firebase/pages/todos/[id].js
@@ -0,0 +1,19 @@
+import { Grid } from '@mui/icons-material'
+import React from 'react'
+
+const Detail = () => {
+  return (
+    <Grid
+    container
+    spacing={0}
+    direction="column"
+    alignItems="center"
+    justifyContent="center"
+    style={{ minHeight: '100vh' }}
+    >
+        details
+    </Grid>
+  )
+}
+
+export default Detail

ccd220509d74032fc939ba8d12a148217a4278fd
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-25 20:18:45 +0500

    Commited File : Todo.js at 7/25/2022 8:18:45 PM

--- nextcrudapp-firebase/components/Todo.js
+++ nextcrudapp-firebase/components/Todo.js
@@ -5,16 +5,24 @@ import moment from 'moment'
 import React, { useContext } from 'react'
 import { TodoContext } from '../pages/TodoContext'
 import TodoService from '../services/TodoService'
+import { useRouter } from 'next/router'
 
 const Todo = ({id,timestamp,title,description}) => {
-  const { showAlert } = useContext(TodoContext);
+  const { showAlert , setTodo} = useContext(TodoContext);
+  const router = useRouter();
   const deleteTodo = async (id,e) => {
     e.stopPropagation();
     await TodoService.deleteTodo('todos',id);
     showAlert('error',`Todo deleted successfully. ID: ${id}`);
   } 
+
+  const seeMore = (id,e) => {
+    e.stopPropagation();  
+    router.push(`/todos/${id}`);
+  }
   return (
     <ListItem
+    onClick={() => setTodo({id,title,description,timestamp})}
     sx={{mt:3,boxShadow: 3}}
     style={{backgroundColor: '#fff'}}
     secondaryAction={
@@ -22,7 +30,7 @@ const Todo = ({id,timestamp,title,description}) => {
         <IconButton onClick={e => deleteTodo(id,e)}>
           <DeleteIcon/>
         </IconButton>
-        <IconButton>
+        <IconButton onClick={e => seeMore(id,e)}>
           <MoreVertIcon/>
         </IconButton>
       </>
